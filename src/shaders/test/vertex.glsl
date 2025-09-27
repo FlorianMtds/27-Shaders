3d4e318b5ce8b2c1abef74e4e9f63c2085617c12
Florian <florianmtddev@gmail.com>
2025-09-27 13:59:21 +0200

    glsl shaders configuration

--- src/shaders/test/vertex.glsl
+++ src/shaders/test/vertex.glsl
@@ -1,12 +1,10 @@
-vertexShader: `
-    uniform mat4 projectionMatrix;
-    uniform mat4 viewMatrix;
-    uniform mat4 modelMatrix;
+uniform mat4 projectionMatrix;
+uniform mat4 viewMatrix;
+uniform mat4 modelMatrix;
 
-    attribute vec3 position;
+attribute vec3 position;
 
-    void main()
-    {
-        gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(position, 1.0);
-    }
-`,
+void main()
+{
+    gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(position, 1.0);
+}
