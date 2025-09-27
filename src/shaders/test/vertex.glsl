d699ca85a2163f240dca404918f2385c7b362481
Florian <florianmtddev@gmail.com>
2025-09-27 14:36:46 +0200

    aRandom and vRandom

--- src/shaders/test/vertex.glsl
+++ src/shaders/test/vertex.glsl
@@ -3,16 +3,22 @@ uniform mat4 viewMatrix; // Apply transformations relative to the camera (positi
 uniform mat4 modelMatrix; // Apply transformations relative to the MEsh (position, rotation, scale)
 
 attribute vec3 position;
+attribute float aRandom;
+
+varying float vRandom;
 
 void main()
 {
     vec4 modelPosition = modelMatrix * vec4(position, 1.0);
-    modelPosition.z += sin(modelPosition.x * 10.0) * 0.1;
+    // modelPosition.z += sin(modelPosition.x * 10.0) * 0.1;
+    modelPosition.z += aRandom * 0.1;
 
     vec4 viewPosition = viewMatrix * modelPosition;
     vec4 projectedPosition = projectionMatrix * viewPosition;
 
     gl_Position = projectedPosition;
+
+    vRandom = aRandom;
 
     // gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(position, 1.0);
 }
